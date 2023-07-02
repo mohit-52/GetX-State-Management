1847d3c3d1600f27e40bda0ec4f488a44d6d4376
Mohit <mohitkaushis7@gmail.com>
2023-07-02 09:33:59 +0530

    Light & Dark Themes Added

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -12,19 +12,78 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("GetX Tutorial"),),
-      body: const Column(
+      appBar: AppBar(
+        title: const Text("GetX Tutorial"),
+      ),
+      body: Column(
         children: [
+          Card(
+            child: ListTile(
+              title: Text("Getx Dialog Alert"),
+              subtitle: Text("Dialog Alert With GetX"),
+              onTap: () {
+                Get.defaultDialog(
+                    title: "Delete Chat",
+                    middleText: "Are you sure you want to delete this chat",
+                    confirm: TextButton(onPressed: () {}, child: Text('Ok')),
+                    cancel: TextButton(
+                        onPressed: () {
+                          Get.back();
+                        },
+                        child: Text("Cancel")));
+              },
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text("GetX Bottom Sheet"),
+              subtitle: Text("Bottom Sheet With GetX"),
+              onTap: () {
+                Get.bottomSheet(
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.redAccent,
+                        borderRadius: BorderRadius.circular(25)),
+                    child: Column(
+                      children: [
+                        ListTile(
+                          leading: Icon(Icons.light_mode),
+                          title: Text("Light Theme"),
+                          onTap: () {
+                            print("Tapped Light");
+                            Get.changeTheme(ThemeData.light(useMaterial3: true));
+                            setState(() {
+
+                            });
+                          },
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.dark_mode),
+                          title: Text("Dark Theme"),
+                          onTap: () {
+                            print("Tapped Dark");
+                            Get.changeTheme(ThemeData.dark(useMaterial3: true));
+                            setState(() {
+
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          )
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          Get.snackbar(
-              "Flutter",
-              "This is Mohit's Project",
+        onPressed: () {
+          Get.snackbar("Flutter", "This is Mohit's Project",
               snackPosition: SnackPosition.BOTTOM,
               backgroundColor: Colors.white12.withOpacity(0.5),
-          mainButton: TextButton(onPressed: (){}, child: const Icon(Icons.ad_units_sharp)));
+              mainButton: TextButton(
+                  onPressed: () {}, child: const Icon(Icons.ad_units_sharp)));
         },
         child: const Icon(Icons.add),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -14,27 +14,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Flutter Demo',
+      themeMode: ThemeMode.system,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        // primarySwatch: Colors.blue,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
-        useMaterial3: false,
+        primarySwatch: Colors.blue,
+        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
+        // useMaterial3: false,
       ),
-      home: const HomeScreen(),
+      home:  HomeScreen(),
     );
   }
 }
